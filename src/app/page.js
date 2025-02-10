d82a82a738cd18301c74dcb6176da1e09ba3bc0a
shibenchigogo <shibenchi@gmail.com>
2025-02-11 02:59:08 +0800

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -203,7 +203,7 @@ const HomePage = () => {
               </div>
               <button
                 onClick={() => {
-                  navigator.clipboard.writeText('6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPQ');
+                  navigator.clipboard.writeText('2SDZfr5aFs3cDmca1aroesSy5zrakdUnLpULrcUNpump');
                 }}
                 className="px-4 py-2 bg-blue-600/20 hover:bg-blue-600/30 border border-blue-500/30
                           rounded-lg transition-all duration-300 hover:scale-105 flex items-center gap-2"
